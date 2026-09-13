08e75e79a4ef06ec9e879e522ca406c954699dac
FelipeMatta <felipeobmatta@hotmail.com>
2026-09-13 17:01:18 -0300

    fix: correção da localização da pasta js

---
+++ static/js/termos.js
@@ -0,0 +1,8 @@
+function abrirPolitica() {
+    document.getElementById("modalPolitica").style.display = "flex";
+}
+
+function fecharPolitica() {
+    document.getElementById("modalPolitica").style.display = "none";
+}
+
