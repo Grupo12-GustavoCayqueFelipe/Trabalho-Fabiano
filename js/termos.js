660b8862c1c5c500b60a71b14139a09cea88ef88
FelipeMatta <felipeobmatta@hotmail.com>
2026-09-13 17:06:13 -0300

    fix: corrige caminho do CSS de termos e remove pasta js duplicada do repositorio

--- js/termos.js
+++
@@ -1,8 +0,0 @@
-function abrirPolitica() {
-    document.getElementById("modalPolitica").style.display = "flex";
-}
-
-function fecharPolitica() {
-    document.getElementById("modalPolitica").style.display = "none";
-}
-
